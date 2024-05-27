c724846ac76129b46698d176eaa7c662038b7b31
Sovinha <161521371+Sovinha@users.noreply.github.com>
2024-05-27 18:48:43 -0300

    Delete demo directory

--- demo/src/main/java/repository/FilmeRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.Filme;
-
-public interface FilmeRepository extends JpaRepository<Filme, Long> {
-}
